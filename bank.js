17476b61270cee5d953918c43f3235b9bed29bb8
Masaki Fukumoto <fm8624@gmail.com>
2023-03-13 19:16:48 +0900

    created page2 & 3 function

--- bank.js
+++ bank.js
@@ -45,4 +45,148 @@ function initializeUserAccount() {
     parseInt(form.querySelectorAll(`input[name="userFirstDeposit"]`).item(0).value)
   );
   console.log(userBankAccount);
+
+  // 1ページ目非表示
+  config.initialForm.classList.add('d-none');
+
+  // 2ページ目の呼び出し
+  config.bankPage.append(mainBankPage(userBankAccount));
+}
+
+// page2 を作成する関数
+function mainBankPage(bankAccount) {
+  let infoCon = document.createElement('div');
+  infoCon.classList.add('text-end', 'mb-4', 'px-4');
+
+  let nameP = document.createElement('p');
+  nameP.classList.add('font-size', 'm-3', 'p-1');
+
+  // namePと全く同じクラスを持っているのでコピー
+  let bankIdP = nameP.cloneNode(true);
+  let initialDepositP = nameP.cloneNode(true);
+
+  // オブジェクトの挿入
+  nameP.innerHTML = bankAccount.getFullName();
+  bankIdP.innerHTML = bankAccount.accountNumber;
+  initialDepositP.innerHTML = `$${bankAccount.initialDeposit}`;
+
+  infoCon.append(nameP, bankIdP, initialDepositP);
+
+  let balanceCon = document.createElement('div');
+  balanceCon.classList.add('px-3');
+  balanceCon.innerHTML = `
+    <div class="d-flex justify-content-center bg-danger col-12 py-1 py-md-2">
+      <p class="col-6 text-start font-size px-4">Available Balance</p>
+      <p class="col-6 text-end font-size px-4">$${bankAccount.money}</p>
+    </div>
+    `;
+
+  let menuCon = document.createElement('div');
+  menuCon.classList.add('d-flex', 'justify-content-center', 'flex-wrap', 'text-center', 'py-1', 'px-3');
+  menuCon.innerHTML = `
+    <div class="col-lg-4 col-12 py-1 py-md-3 px-md-1">
+      <div id="withdrawBtn" class="bg-blue hover p-4">
+        <p class="font-size mb-3">WITHDRAWAL</p>
+        <i class="fas fa-wallet fa-3x"></i>
+      </div>
+    </div>
+    <div class="col-lg-4 col-12 py-1 py-md-3 px-md-1">
+      <div id="depositBtn" class="bg-blue hover p-4">
+        <p class="font-size mb-3">DEPOSIT</p>
+        <i class="fas fa-coins fa-3x"></i>
+      </div>
+    </div>
+    <div class="col-lg-4 col-12 py-1 py-md-3 px-md-1">
+      <div id="comeBackLaterBtn" class="bg-blue hover p-4">
+        <p class="font-size mb-3">COME BACK LATER</p>
+        <i class="fas fa-home fa-3x"></i>
+      </div>
+    </div>
+  `;
+
+  menuCon.querySelectorAll('#withdrawBtn')[0].addEventListener('click', function () {
+    alert('withdraw');
+  });
+  menuCon.querySelectorAll('#depositBtn')[0].addEventListener('click', function () {
+    alert('deposit');
+  });
+  menuCon.querySelectorAll('#comeBackLaterBtn')[0].addEventListener('click', function () {
+    alert('come back later');
+  });
+
+  let container = document.createElement('div');
+  container.append(infoCon, balanceCon, menuCon);
+
+  return container;
+}
+
+// page3を作成する関数
+function billInputSelector(title) {
+  let container = document.createElement('div');
+  container.innerHTML = `
+    <h2 class="text-center fw-bold pb-3">${title}</h2>
+
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$100</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="5" />
+        </div>
+      </div>
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$50</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="1" />
+        </div>
+      </div>
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$20</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="2" />
+        </div>
+      </div>
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$10</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="3" />
+        </div>
+      </div>
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$5</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="1" />
+        </div>
+      </div>
+      <div class="form-group row pb-3">
+        <div class="col-3">
+          <h5 class="text-center py-1">$1</h5>
+        </div>
+        <div class="col-9">
+          <input type="number" class="form-control text-end" placeholder="4" />
+        </div>
+      </div>
+
+      <div class="money-box bg-orient col-12 p-3">
+        <h5 class="text-center text-light mb-0">$0.00</h5>
+      </div>
+
+      <div class="row pt-3">
+        <div class="col-6">
+          <button class="btn btn-outline-primary fw-bold col-12 p-3">Go back</button>
+        </div>
+        <div class="col-6">
+          <button class="btn btn-primary fw-bold col-12 p-3">Next</button>
+        </div>
+      </div>
+  `;
+  return container;
 }
